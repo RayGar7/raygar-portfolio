9c8380e7c4f68862e138a9608bc3b6663328e790
RayGar7 <rgarcia.7770@gmail.com>
2025-11-09 20:28:33 -0600

    redeploy fix attempt

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,12 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
   output: 'export',
   trailingSlash: true,
   images: {
     unoptimized: true
   },
-  // Only add basePath if you're deploying to a subdirectory (username.github.io/repo-name)
-  // Remove these lines if deploying to username.github.io directly
-  basePath: '/raygar-portfolio',
-  assetPrefix: '/raygar-portfolio/',
-}
+  reactCompiler: true,
+};
 
-module.exports = nextConfig
+export default nextConfig;
